60aac195c6aefff1f674b3b51dcf2a518547ec56
mochamad rafli <rafliforbusiness@gmail.com>
2023-01-15 18:11:37 +0700

    improvement using sed

--- environment.sh
+++ environment.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && sed -i -e 's/$DESTINATION/'"$DESTINATION"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+sed -i -e 's/$PORT/'"$PORT"'/g;s/$DESTINATION/'"$DESTINATION"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
